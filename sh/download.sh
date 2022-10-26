41650a1298af2fcbe53564067f0d850157abb91e
Gioxx <giovanni@solone.it>
2022-10-26 14:14:49 +0200

    Update download.sh

--- sh/download.sh
+++ sh/download.sh
@@ -1,6 +1,7 @@
-wget -c "https://raw.githubusercontent.com/gioxx/xfiles/master/experimental.txt" -O "lists/experimental.txt"
-wget -c "https://raw.githubusercontent.com/gioxx/xfiles/master/facebook.txt" -O "lists/facebook.txt"
-wget -c "https://raw.githubusercontent.com/gioxx/xfiles/master/filtri.txt" -O "lists/filtri.txt"
-wget -c "https://raw.githubusercontent.com/gioxx/xfiles/master/fritz/fritz_blacklist.txt" -O "lists/fritz_blacklist.txt"
-wget -c "https://raw.githubusercontent.com/gioxx/xfiles/master/siteblock.txt" -O "lists/siteblock.txt"
-wget -c "https://raw.githubusercontent.com/gioxx/xfiles/master/upd.txt" -O "lists/upd.txt"
+rm -f lists/*
+wget "https://raw.githubusercontent.com/gioxx/xfiles/master/experimental.txt" -O "lists/experimental.txt"
+wget "https://raw.githubusercontent.com/gioxx/xfiles/master/facebook.txt" -O "lists/facebook.txt"
+wget "https://raw.githubusercontent.com/gioxx/xfiles/master/filtri.txt" -O "lists/filtri.txt"
+wget "https://raw.githubusercontent.com/gioxx/xfiles/master/fritz/fritz_blacklist.txt" -O "lists/fritz_blacklist.txt"
+wget "https://raw.githubusercontent.com/gioxx/xfiles/master/siteblock.txt" -O "lists/siteblock.txt"
+wget "https://raw.githubusercontent.com/gioxx/xfiles/master/upd.txt" -O "lists/upd.txt"
